599932d02e6ab6e290efb3221f3d45ba65e3b197
yt6983138 <yt6983138@gmail.com>
2023-12-09 12:43:23 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace StopWatchOnGlobalMouseClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TimeTextBox = new System.Windows.Forms.TextBox();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.StartOrStopDetecting = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TimeTextBox
+            // 
+            this.TimeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.TimeTextBox.Location = new System.Drawing.Point(12, 12);
+            this.TimeTextBox.Name = "TimeTextBox";
+            this.TimeTextBox.ReadOnly = true;
+            this.TimeTextBox.Size = new System.Drawing.Size(250, 44);
+            this.TimeTextBox.TabIndex = 0;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(12, 62);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 1;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // StartOrStopDetecting
+            // 
+            this.StartOrStopDetecting.Location = new System.Drawing.Point(187, 62);
+            this.StartOrStopDetecting.Name = "StartOrStopDetecting";
+            this.StartOrStopDetecting.Size = new System.Drawing.Size(75, 23);
+            this.StartOrStopDetecting.TabIndex = 2;
+            this.StartOrStopDetecting.Text = "Start Detect";
+            this.StartOrStopDetecting.UseVisualStyleBackColor = true;
+            this.StartOrStopDetecting.Click += new System.EventHandler(this.StartOrStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 277);
+            this.Controls.Add(this.StartOrStopDetecting);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.TimeTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TimeTextBox;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button StartOrStopDetecting;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatchOnGlobalMouseClick
+{
+    public partial class Form1 : Form
+    {
+        private volatile bool DetectingStarted = false;
+        private volatile Stopwatch MyStopwatch;
+        private Thread worker;
+        public Form1()
+        {
+            MyStopwatch = new Stopwatch();
+            InitializeComponent();
+            worker = new Thread(new ThreadStart(CheckMouseDownAllTheTime));
+            worker.Start();
+        }
+
+
+        private void StartOrStop_Click(object sender, EventArgs e)
+        {
+            this.DetectingStarted = !DetectingStarted;
+            if (this.DetectingStarted)
+            { // stop
+                StartOrStopDetecting.Text = "Stop Detect";
+                Thread.Sleep(50);
+                lock (this.MyStopwatch)
+                {
+                    this.MyStopwatch.Stop();
+                }
+            }
+            else
+            { // start
+                StartOrStopDetecting.Text = "Start Detect";
+            }
+        }
+        private void CheckMouseDownAllTheTime()
+        {
+            Thread.Sleep(500);
+            while (true)
+            {
+                Thread.Sleep(20);
+                //Console.WriteLine(GetAsyncKeyState(0x01));
+                if (!this.DetectingStarted)
+                {
+                    lock (this.MyStopwatch)
+                    {
+                        this.MyStopwatch.Stop();
+                    }
+                }
+                if (this.DetectingStarted && GetAsyncKeyState(0x01) == -32767)
+                {
+                    lock (this.MyStopwatch)
+                    {
+                        if (this.MyStopwatch.IsRunning)
+                        {
+                            this.MyStopwatch.Stop();
+                        }
+                        else
+                        {
+                            this.MyStopwatch.Start();
+                        }
+                    }
+                }
+                this.TimeTextBox.Invoke((MethodInvoker)delegate
+                {
+                    this.TimeTextBox.Text = this.MyStopwatch.Elapsed.ToString("c");
+                });
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            lock (this.MyStopwatch)
+            {
+                this.MyStopwatch.Reset();
+            }
+        }
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int VirtualKeyPressed);
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.worker.Abort();
+        }
+    }
+}
